8251d665887f32f9c6ce3345c8074a9ab5ffc051
rodalschulz <rod.schulz.rosas@gmail.com>
2024-05-09 15:56:26 -0500

    change fetching url inside SDK file to new backend url

--- src/sdk_backend_fetch.js
+++ src/sdk_backend_fetch.js
@@ -1,5 +1,6 @@
-const baseURL =
-  "https://backenddummy-rodalschulz-rodschulzs-projects.vercel.app";
+const baseURL = "https://backenddummy2-rodschulzs-projects.vercel.app";
+// "https://backenddummy-rodalschulz-rodschulzs-projects.vercel.app";
+// "http://localhost:3000";
 const version = "v1";
 
 export const getUsernameData = async () => {
